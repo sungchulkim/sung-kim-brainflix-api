18ea8e1d39594f6ed2119eb8f7467b1bdfcb946c
sungchulkim <37005088+sungchulkim@users.noreply.github.com>
2024-08-31 07:41:12 -0700

    Added index.js and folders data public routes

---
+++ index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import videosRoutes from './routes/videos.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use('/images', express.static('public/images'));
+app.use('/videos', videosRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
