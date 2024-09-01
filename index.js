0e17670a40bd720cdcc1652651cb868874747e6f
sungchulkim <37005088+sungchulkim@users.noreply.github.com>
2024-08-31 21:00:52 -0700

    Adjusted static of index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 8080
 app.use(cors())
 app.use(express.json())
 app.use('/images', express.static('public/images'))
-app.use('/upload', express.static('public/videos'))
 app.use('/videos', videosRoutes)
 
 app.listen(PORT, () => {
